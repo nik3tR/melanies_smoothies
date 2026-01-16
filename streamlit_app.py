8e2827749e60f960b9bf232f99d72e754e07f7c3
nik3tR <84939453+nik3tR@users.noreply.github.com>
2026-01-16 15:08:07 -0500

    Update search value display for selected fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ if ingredients_list:
         ingredients_string += fruit + ' '
       
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.write('The search value for ', fruit,' is ', search_on, '.')
       
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
