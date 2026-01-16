ccce914f17622685bc7a653401a57b10c63631a3
nik3tR <84939453+nik3tR@users.noreply.github.com>
2026-01-16 15:00:45 -0500

    Fix commented-out line for fetching fruit options

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ st.write("Name on your smoothie will be: ", name_on_order)
 # Get the current credentials
 cnx = st.connection("snowflake")
 session = cnx.session()
-#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
